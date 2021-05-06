96c81eae210255eee790155b05c31719703eea28
farzadgo <farzadesign@gmail.com>
2021-05-06 20:18:55 +0200

    404 inline styled

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import * as styles from '../styles/pages/NotFound.module.css'
 import { Helmet } from 'react-helmet'
 
 const NotFound = () => {
   const info = { directory: '', workTitle: ''}
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    fontFamily: 'var(--code-font)',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    lineHeight: '1.8em',
+  }
+
   return (
     <Layout info={info}>
       <Helmet title="Not Found" />
       <main className="main">
-        <div className={styles.body}>
+        <div style={style}>
           <p>Page not found!</p>
           <p>Click ☰ for more</p>
         </div>
